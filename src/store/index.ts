5185932da3c07ac47195fccf7c3ba0eec3c6f359
unknown <italoasouzat.i@gmail.com>
2020-09-15 09:36:18 -0300

    mostrando valores

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
+import { ICartState } from './modules/cart/types';
 
+export interface IState {
+  cart: ICartState;
+}
 
 //Store disponibiliza todo conteudo pra o app
 const store = createStore(rootReducer);
